57bc785958266a95775ba838544d2ef5a8c8741e
nihat-js <abdullazadenihat@gmail.com>
2023-02-25 02:01:18 +0400

    profile page bug fixed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,44 +17,45 @@ import Nav from '../components/Nav'
 export default function Index() {
 
 
-  
+
   const params = useParams()
   const navigate = useNavigate()
   const target_username = params.username
-  const [isLoading,setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
   const [posts, setPosts] = useState([])
   const [postStyle, setPostStyle] = useState('grid')
-  const [target,setTarget] = useState({})
+  const [target, setTarget] = useState({})
 
 
-
-
-  async function handleMessage(req,res){
+  async function loadPosts() {
+    setPosts("loading")
+    // console.log('zz')
     try {
-      let response = await axios.post("http://localhost:5000/api/chat/start-or-find-chat",{target_username , token : getCookie('token')})
+      let response = await axios.post('http://localhost:5000/api/post/place', { name: "feed", token: getCookie('token') })
+      console.log(response.data)
+      setPosts(response.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  async function handleMessage(req, res) {
+    try {
+      let response = await axios.post("http://localhost:5000/api/chat/start-or-find-chat", { target_username, token: getCookie('token') })
       console.log(response)
       navigate("/chat/" + response.data.chat_id)
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   }
 
-  async function getTargetProfile(){
-    try{  
-      let response = await axios.post("http://localhost:5000/api/get/target-profile",{token : getCookie('token') , target_username : target_username })
+  async function getTargetProfile() {
+    try {
+      let response = await axios.post("http://localhost:5000/api/get/target-profile", { token: getCookie('token'), target_username: target_username })
       setTarget(response.data)
       // console.log(response)
-    }catch(err){
-      console.log(err)
-    }
-  }
-
-  async function getPosts(){
-    try{
-      let response  = await axios.post("http://localhost:5000/api/post/user-posts",{token : getCookie('token')})
-      console.log(response)
-      setPosts(response.data)
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   }
@@ -63,7 +64,8 @@ export default function Index() {
 
   useEffect(() => {
     getTargetProfile()
-    getPosts()
+    loadPosts()
+    // getPosts()
   }, [])
 
 
@@ -75,12 +77,12 @@ export default function Index() {
 
 
       <section className="start">
-        <div style={{maxWidth : "1200px"}}  className="mx-auto flex flex-col items-center justify-between ">
+        <div style={{ maxWidth: "1200px" }} className="mx-auto flex flex-col items-center justify-between ">
           <div className="row mx-auto flex flex-col content-center" style={{ maxWidth: "800px" }}>
-            <div style={{ minWidth : "800px", height:"200px"}} className="cover bg-slate-200 rounded-lg">
+            <div style={{ minWidth: "800px", height: "150px" }} className="cover bg-slate-200 rounded-lg">
             </div>
             <div className="img-wrap mx-auto">
-              <Avatar className="w-16 mt-10"  avatar={target.avatar} username={target.username} />              
+              <Avatar className="w-16 mt-10" avatar={target.avatar} username={target.username} />
             </div>
             <h3 className="username font-bold text-2xl"> {target.username} </h3>
             <div className="stats">
@@ -94,11 +96,13 @@ export default function Index() {
               <span>  {target.createdAt} </span>
             </div>
             <div className="actions flex gap-2">
-              <FollowButton isFollowing={target.isFollowing}  target_username={target_username} />
+              <FollowButton isFollowing={target.isFollowing} target_username={target_username} />
               <button className='px-2 py-3 bg-danube-600 font-bold text-white rounded-md' onClick={handleMessage} > Message </button>
             </div>
           </div>
         </div>
+
+{/*         
         <section className="test py-20">
           <div style={{ maxWidth: "1000px" }} className="container mx-auto">
             <hr />
@@ -107,17 +111,21 @@ export default function Index() {
                 <img className='w-5' src={gridSvg} alt="" />
                 <span className='text-sm'>Grid style</span>
               </div>
-              <div className={`flex gap-1 py-2 px-3 cursor-pointer hover:bg-danube-600 hover:text-white    ${postStyle == "row" ? "font-bold" : "text-gray-600"} ` }>
+              <div className={`flex gap-1 py-2 px-3 cursor-pointer hover:bg-danube-600 hover:text-white    ${postStyle == "row" ? "font-bold" : "text-gray-600"} `}>
                 <img className='w-5' src={rowSvg} alt="" />
                 <span className='text-sm'> Row style </span>
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         <section className="posts py-10">
-          <div style={{maxWidth : "600px"}}  className="mx-auto" >
-            { posts.map((item,index) => <PostBox item={item} key={index} />  )  }
+          <div style={{ maxWidth: "600px" }} className="mx-auto" >
+
+          { 
+          typeof(posts) == "object"  ? posts.map((item, index) => <PostBox key={index} data={item} />)  :
+          <PostSkleton />
+          }
           </div>
         </section>
 
